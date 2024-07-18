2cd529c00a3ee4765aed774dce7be334149cad1c
ArchebasovVl <archebasovvl@gmail.com>
2024-07-18 15:49:49 +0300

    Add monitor
    * get_players() is redesigned to return list of players for /players and /monitor.
    * application uses jobQueue which must be installed via !!! pip install python-telegram-bot[job-queue] !!!
    * request interval is set to 240 seconds to approximately match the server data refresh

--- bot.py
+++ bot.py
@@ -1,42 +1,82 @@
 import requests
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, ApplicationBuilder
-
+from telegram.ext import CommandHandler, CallbackContext, ApplicationBuilder, JobQueue
 
 TELEGRAM_BOT_TOKEN = ''
 SERVER_URL = "https://api.mcsrvstat.us/3/d11.gamely.pro:20187"
+REQUEST_INTERVAL = 240
+online_players = []
 
-def get_players():
-    try:
-        response = requests.get(SERVER_URL)
-        data = response.json()
-        
-        if 'players' in data and 'online' in data['players']:
-            online_players = data['players']['online']
-            if online_players > 0 and 'list' in data['players']:
-                players = [player['name'] for player in data['players']['list']]
-                return f"В данный момент на сервере находятся игроки: {', '.join(players)}"
-            else:
-                return "На сервере в данный момент нет игроков."
+
+def get_players() -> (list[str], None):
+    response = requests.get(SERVER_URL)
+    data = response.json()
+    if 'players' in data and 'online' in data['players']:
+        online_players_count = data['players']['online']
+        if online_players_count > 0 and 'list' in data['players']:
+            return [player['name'] for player in data['players']['list']]
         else:
-            return "Не удалось получить информацию о игроках."
-    except Exception as e:
-        return f"Произошла ошибка при запросе к серверу: {e}"
+            return []
+    else:
+        return None
+
+
+async def check_new_players(context: CallbackContext) -> None:
+    global online_players
+    current_players = get_players()
+    if current_players is None:
+        return
+    joined_players = set(current_players) - set(online_players)
+    quited_players = set(online_players) - set(current_players)
+    online_players = current_players
+    if joined_players or quited_players:
+        reply = (f'{"На сервер зашли игроки: " + ", ".join(joined_players) + ". " if joined_players else ""}'
+                 f'{"С сервера вышли: " + ", ".join(quited_players) if quited_players else ""}')
+        await context.bot.send_message(context.job.chat_id, reply)
+
 
 async def start(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text('Привет! Используй команду /players, чтобы узнать кто сейчас на сервере Minecraft.')
 
+
 async def players(update: Update, context: CallbackContext) -> None:
-    players_info = get_players()
-    await update.message.reply_text(players_info)
+    global online_players
+    try:
+        online_players = get_players()
+        if online_players is None:
+            reply = 'Не удалось получить информацию о игроках.'
+        elif online_players:
+            reply = f'В данный момент на сервере находятся игроки: {", ".join(online_players)}'
+        else:
+            reply = 'На сервере в данный момент нет игроков.'
+    except Exception as e:
+        reply = f"Произошла ошибка при запросе к серверу: {e}"
+    await update.message.reply_text(reply)
+
+
+async def monitor(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+    context.job_queue.run_repeating(check_new_players, interval=REQUEST_INTERVAL, chat_id=chat_id)
+    await update.message.reply_text(
+        'Коллеги! Просьба не опаздывать на пары! '
+        'Теперь я слежу за всеми входящими и выходящими в аудиторию.'
+    )
+
+
+async def monitor_stop(update: Update, context: CallbackContext) -> None:
+    await context.application.job_queue.stop()
+    await update.message.reply_text('Свободная посещаемость! я не слежу за вами.')
+
 
 def main() -> None:
-    application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+    application = ApplicationBuilder().job_queue(JobQueue()).token(TELEGRAM_BOT_TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("monitor", monitor))
+    application.add_handler(CommandHandler("monitor_stop", monitor_stop))
     application.add_handler(CommandHandler("players", players))
-
     application.run_polling()
+
 
 if __name__ == '__main__':
     main()
